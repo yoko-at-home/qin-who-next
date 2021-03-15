d1dd0e41e343543b768be8ceeeace45e01061980
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-03-16 07:21:50 +0900

    added sound

--- script.js
+++ script.js
@@ -17,19 +17,19 @@ textarea.addEventListener('keyup', (e) => {
 })
 
 function createTags(input) {
-  console.log(input)
+  console.log(input);
   const tags = input
     .split('@')
     .filter((tag) => tag.trim() !== '')
     .map((tag) => tag.trim());
-  console.log(tags)
-  tagsEl.innerHTML = ''
-  tags.forEach(tag => {
-    const tagEl = document.createElement('span')
-    tagEl.classList.add('tag')
-    tagEl.innerText = tag
-    tagsEl.appendChild(tagEl)
-  })
+  // console.log(tags)
+  tagsEl.innerHTML = '';
+  tags.forEach((tag) => {
+    const tagEl = document.createElement('span');
+    tagEl.classList.add('tag');
+    tagEl.innerText = tag;
+    tagsEl.appendChild(tagEl);
+  });
 }
 
 function randomSelect() {
